0b5ad6d3db85e0ccf320e260eb5e0dab4787fa60
maivi <maximilianokadyszyn@gmail.com>
2017-05-12 18:03:38 -0300

    Agregada la API KEY y el mapa a la vista

--- application/views/layouts/common/main.php
+++ application/views/layouts/common/main.php
@@ -4,6 +4,12 @@
 	<meta charset="UTF-8">
 	<title><?php echo $titulo; ?></title>
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>">
+	<style>
+		#map {
+			height: calc(100vh - 120px);
+			width: 100%;
+		}
+	</style>
 </head>
 <body>
 	<?php if ($header) echo $header ?>
@@ -14,5 +20,22 @@
 	
 	<script src="<?php echo base_url("assets/js/jquery.js"); ?>"></script>
 	<script src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+	 <script>
+      function initMap() {
+        var uluru = {lat: -34.9950075, lng: -67.5100458};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 18,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDdbzPyzRdoXfcf-G_IAlFXgukEWqdr5uI&callback=initMap">
+    </script>
+
 </body>
 </html>
