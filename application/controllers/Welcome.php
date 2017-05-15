0e64c7c6619d3d01841cf75e667dba0c6f46aa7d
maivi <maximilianokadyszyn@gmail.com>
2017-05-15 10:59:22 -0300

    Reestructuracion

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -6,11 +6,11 @@ class Welcome extends CI_Controller {
 	public function index()
 	{
 		$data["titulo"] = "UNC";
-		$this->load->view('layouts/common/head',$data);
-		$this->load->view('layouts/common/style');
-		$this->load->view('layouts/common/header');
-		$this->load->view('layouts/common/main');
-		$this->load->view('layouts/common/scripts');
-		$this->load->view('layouts/common/footer');
+		$this->load->view('layouts/head',$data);
+		$this->load->view('layouts/style');
+		$this->load->view('layouts/header');
+		$this->load->view('layouts/main');
+		$this->load->view('layouts/scripts');
+		$this->load->view('layouts/footer');
 	}
 }
