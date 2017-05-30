e6e95aa4401b0eb513915e823652bdcfec6bc1f0
maivi <maximilianokadyszyn@gmail.com>
2017-05-30 09:50:52 -0300

    Agregada la nueva estructura de mapas

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,7 +9,12 @@ class Welcome extends CI_Controller {
 		$this->load->view('layouts/head',$data);
 		$this->load->view('layouts/style');
 		$this->load->view('layouts/header');
+		$this->load->view('start_body');
+		$this->load->view('ventana_flotante');
 		$this->load->view('main');
+		$this->load->view('registro');
+		$this->load->view('login');
+		$this->load->view('end_body');
 		$this->load->view('layouts/scripts');
 		$this->load->view('layouts/footer');
 	}
--- application/views/main.php
+++ application/views/main.php
@@ -1,5 +1,3 @@
-<body>
-	
-	<div id="map"></div>
+<div id="map"></div>
 
-</body>
+
