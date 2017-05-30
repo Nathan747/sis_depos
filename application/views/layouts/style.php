38c009f45ccd3f724fbc7c0933aedcba9946df5f
maivi <maximilianokadyszyn@gmail.com>
2017-05-30 10:05:00 -0300

    Agregado style.css

--- application/views/layouts/style.php
+++ application/views/layouts/style.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>">
+<link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
 <style>
 	body{
 		position: relative;
