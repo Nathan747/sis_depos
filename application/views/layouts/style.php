e7a6c76c5dc588a6673ca42a490796837bee672b
Nathan747 <escorpion_erc@hotmail.com>
2017-10-25 16:09:09 -0300

    responsive

--- application/views/layouts/style.php
+++ application/views/layouts/style.php
@@ -3,84 +3,126 @@
 <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,700" rel="stylesheet">
 <style>
 .name2 {
-    top: 10px;
-    position: relative;
-    font-size: 20px;
-    padding-left: 46px;
+	top: 10px;
+	position: relative;
+	font-size: 20px;
+	padding-left: 46px;
 }
 
 .job2 {
-    font-weight: 400!important;
-    line-height: 1;
-    color: #777;
-    padding-top: 10px;
-    padding-left: 46px;
+	font-weight: 400!important;
+	line-height: 1;
+	color: #777;
+	padding-top: 10px;
+	padding-left: 46px;
 }
 
 
 .first2{
-    padding-left: 46px;
+	padding-left: 46px;
 }
 
 .container2{
-    background:#22205F;
-    width: 100%;
-    height: 100px;
-    border-bottom: 1px solid #e7e7e7;
+	background:#22205F;
+	width: 100%;
+	height: 100px;
+	border-bottom: 1px solid #e7e7e7;
 }
 
 .titl{
-color:white;
+	color:white;
 
 }
 
 .container2 .page-header {
-    padding-bottom: 21px;
-    margin: 40px 0 20px;
-    border-bottom: 0px solid #eee!important;
+	padding-bottom: 21px;
+	margin: 40px 0 20px;
+	border-bottom: 0px solid #eee!important;
 }
 
 .profile2 .bio2{
-width: 71%;
+	width: 71%;
 }
 
 
 .profile2{
-border-left:0px solid #cccccc!important;
+	border-left:0px solid #cccccc!important;
 }
 
 .formul {
-    top: 0px;
+	top: 0px;
 }
 
 
 .profile {
 
-    right: -350%;
-  
+	right: -350%;
+
 }
 
 .contenedor-donar {
 
-    right: -250%;
+	right: -250%;
 
 }
 
 .contenedor-registro, .contenedor-donar {
 
-    right: -300%;
+	right: -300%;
 
 }
 
 /*responsive*/
 
-@media(max-width:480px) {
-
-.float{
-display:none;
-
+@media(max-width:1199px) and (min-width: 992px) {
+	.profile {
+		width: 1000px;
+		overflow:scroll;
+	}
 }
 
+
+@media(max-width:991px) and (min-width: 768px) {
+	.profile {
+		width: 778px;
+		overflow:scroll;
+	}
+
+	.contenedor-informacion-perfil {
+		float: none;
+		overflow: hidden;
+		height: 223px;
+	}
+}
+
+
+@media(max-width:767px) {
+	.float{
+		display:none;
+	}
+	.float2{
+		display:none;
+	}
+	.profile {
+		width: 100%;
+		overflow:scroll;
+	}
+	.page-header{
+		font-size: 20px!important;
+	}
+	.photo{
+		max-width: 320px;
+		margin: 0 auto;
+	}
+	.bio2{
+	}
+	.bio{
+	}
+	.contenedor-informacion-perfil {
+		float: none;
+		overflow: hidden;
+		height: 223px;
+	}
 
 }
 
