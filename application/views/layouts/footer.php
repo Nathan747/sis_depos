a8fe92bc85b69ce3ef1ce505732e54038a377483
maivi <maximilianokadyszyn@gmail.com>
2017-11-28 15:10:52 -0300

    Footer y la ID de Facebook

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -2,14 +2,11 @@
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 top-footer">
-				<div class="contenedor-logo-footer">
-					<img src="<?php echo base_url("assets/img/footer-uncuyo.png") ?>" alt="">
+				<div class="texto-footer-izquierda">
+					<span>UNCUYO.</span> Universidad Nacional de Cuyo
 				</div>
-				<div class="info-footer">
-					<p>Centro Universitario, Ciudad de Mendoza.</p>
-					<p>Provincia de Mendoza, Argentina</p>
-					<p>CP M5502|MA</p>
-					<p>+54 261 4135000</p>
+				<div class="texto-footer-derecha">
+					Diseño y desarrollo CICUNC.
 				</div>
 			</div>	
 		</div>
