6a489999b0b893b4b086c6d5b090a68c93a00c68
maivi <maximilianokadyszyn@gmail.com>
2017-11-28 16:35:41 -0300

    Cambiado el footer, enlazado a la UNCUYO

--- application/views/layouts/footer.php
+++ application/views/layouts/footer.php
@@ -3,7 +3,7 @@
 		<div class="row">
 			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 top-footer">
 				<div class="texto-footer-izquierda">
-					<span>UNCUYO.</span> Universidad Nacional de Cuyo
+					<span><a href="http://www.uncuyo.edu.ar/" target="_blank">UNCUYO.</a></span> Universidad Nacional de Cuyo
 				</div>
 				<div class="texto-footer-derecha">
 					Diseño y desarrollo CICUNC.
