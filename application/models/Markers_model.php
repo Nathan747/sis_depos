d94c8a19c4a0cab838acc14ab4a268ac320ad150
maivi <maximilianokadyszyn@gmail.com>
2017-11-30 15:37:16 -0300

    Probando un error que le dio al esteban de servidor

--- application/models/Markers_model.php
+++ application/models/Markers_model.php
@@ -40,15 +40,15 @@ class Markers_model extends CI_Model {
       $json[$i]["biografia_user"] = "";
       $json[$i]["fecha_egreso_user"] = "";
       $json[$i]["pass_user"] = "";
-      $json[$i]["lat_user"] = $row->lat_user;
-      $json[$i]["long_user"] = $row->long_user;
-      $json[$i]["fecha_creacion"] = $row->fecha_creacion;
-      $json[$i]["es_egresado_user"] = $row->es_egresado_user;
-      $json[$i]["jerarquia"] = $row->jerarquia;
-      $json[$i]["facultad_user"] = $row->facultad_user;
-      $json[$i]["carrera_user"] = $row->carrera_user;
-      $json[$i]["img"] = $row->img;
-      $i++;
+      $json[$i]["lat_user"] = "";
+      $json[$i]["long_user"] = "";
+      $json[$i]["fecha_creacion"] = "";
+      $json[$i]["es_egresado_user"] = "";
+      $json[$i]["jerarquia"] = "";
+      $json[$i]["facultad_user"] = "";
+      $json[$i]["carrera_user"] = "";
+      $json[$i]["img"] = "";
+      //$i++;
     }
 
     return $json;
